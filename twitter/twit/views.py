9335753c4ce56c0656f944423d136516f78362fc
Daniil <157521979+GoToProd@users.noreply.github.com>
2024-03-24 14:22:22 +0300

    upd index page
    added index page for check all twits

--- twitter/twit/views.py
+++ twitter/twit/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    
+    context = {
+        'title': 'Twitter - Главная',
+    }
+    
+    return render(request, 'base.html', context)
